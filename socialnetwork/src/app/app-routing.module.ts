da87c42880ccfc41d73eef214b37e1a5ff1187a4
Edoardo Trovarelli <107913556+Edotrova@users.noreply.github.com>
2022-09-19 12:18:16 +0200

    modifica12:18

--- socialnetwork/src/app/app-routing.module.ts
+++ socialnetwork/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) }, { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }, { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }, { path: 'auth/login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) }, { path: 'auth/register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) }];
+const routes: Routes = [
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+ { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'auth/login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) },
+   { path: 'auth/register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
