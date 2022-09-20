b28e0c56564e95916f5da3753a17f806b5d9d47d
Nabbatrox <66868366+Nabbatrox@users.noreply.github.com>
2022-09-20 10:44:43 +0200

    login/register v1.01 testing

--- socialnetwork/src/app/auth.service.ts
+++ socialnetwork/src/app/auth.service.ts
@@ -1,9 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Login } from './imodels/login';
+import { Users } from './models/users';
+
+
+type AuthResponse = {
+
+  accessToken: string,
+  user:Users
+
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
 
-  constructor() { }
+export class AuthService   {
+
+  apiUrl: string = 'http://localhost:3000';
+
+  constructor(private http:HttpClient) {}
+
+  saveAuthToStorage(access:AuthResponse){
+
+    localStorage.setItem('user-access',JSON.stringify(access))
+
+  }
+
+  isUserLogged(): boolean{
+
+    return localStorage.getItem('user-access') != null
+
+  }
+
+  logOut(){
+
+    localStorage.removeItem('user-access')
+
+  }
+
+
+
+  login(loginData:Login){
+
+    return this.http.post<AuthResponse>(this.apiUrl+'/login', loginData)
+
+  }
+
+  
+
 }
--- socialnetwork/src/app/auth/register/register.component.ts
+++ socialnetwork/src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/users.service';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form!:FormGroup;
+
+  constructor( private userSvc: UsersService, private router: Router ) { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required,
+      Validators.pattern("/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/")]),
+      password: new FormControl(null, [Validators.required, Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$')])
+    })
+  }
+
+  signUp(){
+    this.userSvc.add(this.form.value).subscribe(register => {
+      this.form.reset()
+      this.router.navigate([''])
+    })
   }
 
 }
