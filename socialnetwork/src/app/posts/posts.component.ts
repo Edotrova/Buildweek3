51a0884c21eb1a0d4589b5535c95bf4475ce1839
Edoardo Trovarelli <107913556+Edotrova@users.noreply.github.com>
2022-09-20 11:46:17 +0200

    ultimemodifiche2009

--- socialnetwork/src/app/posts/posts.component.ts
+++ socialnetwork/src/app/posts/posts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Posts } from '../models/posts';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  post: Posts = new Posts('','');
+  posts:Posts [] = [];
+  
+
+  constructor(private postsSvc:PostsService, private router: Router) { }
 
   ngOnInit(): void {
+    this.postsSvc.getAll().subscribe(posts => this.posts = posts)
+  }
+
+ 
+
+  // editPost(){
+  //   this.postsSvc.edit(this.post).subscribe(res => {
+  //     let index = this.posts.findIndex(p => p.id === this.post.id)
+  //     this.posts.splice(index,1, this.post)
+  //     this.formAction = 'create'
+  //   })
+  // }
+
+  deletePost(post:Posts){
+    this.postsSvc.delete(post.id).subscribe(res => {
+      let index = this.posts.findIndex(p => p.id === post.id)
+      this.posts.splice(index,1)
+    })
   }
 
 }
